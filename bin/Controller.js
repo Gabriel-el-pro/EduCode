fc4a7844fa69fc0d54a7d57d5028012aa828073c
Gabriel-el-pro <ricovalentina7@gmail.com>
2022-07-29 07:58:21 -0500

    rutas_listas
    Se agregaron todas las rutas necesarias para el proyecto.

--- bin/Controller.js
+++ bin/Controller.js
@@ -220,6 +220,410 @@ deleteGrado(id, res) {
   });
 }
 
+
+getActividad(){
+    //en el modelo User se ejecuta el find sin ninguna condicion...
+    Actividad.find({}, (err, actividades)=>{
+        //en caso de haberse presentado un error se ejecuta el error
+        if(err) throw err;
+        //de lo contrario se retorna un objeto con todos los resultados
+        res.send( { allActividades : actividades } );
+    })
+}
+
+setActividad(actividad, res) {
+    // Se recibe el nuevo usuario en la variable user y se crea a partir del modelo
+    Actividad.create(actividad, function(err, newActividad) {
+        // sihay error se reporta
+        if (err) throw err;
+        // se retorna la informacion con el nuevo usuario creado
+        res.send({ status: 200, nA: newActividad });
+    }); 
+}
+
+//metodo para buscar todos los usuarios
+getActividades(req, res) {
+    //en el modelo User se ejecuta el find sin ninguna condicion...
+    actividad.find({}, (err, actividades) => {
+        //en caso de haberse presentado un error se ejecuta el error
+        if (err) throw err;
+        //de lo contrario se retorna un objeto con todos los resultados
+        res.send({ allactividades: actividades });
+    });
+}
+
+getActividad(id, res) {
+    //en el modelo User se ejecuta el find sin ninguna condicion...
+    Actividad.find({ _id: id }, (err, actividad) => {
+        //en caso de haberse presentado un error se ejecuta el error
+        if (err) throw err;
+        //de lo contrario se retorna un objeto con todos los resultados
+        res.send({ Actividad: actividad });
+    });
+}
+
+updateActividad(actividad, res) {
+    //optenemos los campos que queremos actualizar
+    let { id, id_docente, id_grado, url_icono } = actividad;
+    //actualizamos teniendo en cuenta una condicion con el operador $set
+    //https://docs.mongodb.com/manual/reference/operator/update/set/
+    actividad.updateOne(
+        { _id: id },
+        { $set: { id_docente: id_docente, id_grado: id_grado, url_icono } }
+    )
+        .then(rawResponse => {
+            res.send({ message: "actividad updated", raw: rawResponse });
+        })
+        .catch(err => {
+            if (err) throw err;
+        });
+}
+
+deleteActividad(id, res) {
+    Actividad.deleteOne({ _id: id }, function(err) {
+        if (err) throw err;
+        res.send({ message: "Actividad has been deleted" });
+    });
+}
+
+
+getEvaluacion(){
+    //en el modelo User se ejecuta el find sin ninguna condicion...
+    Evaluacion.find({}, (err, evaluaciones)=>{
+        //en caso de haberse presentado un error se ejecuta el error
+        if(err) throw err;
+        //de lo contrario se retorna un objeto con todos los resultados
+        res.send( { allEvaluaciones : evaluaciones } );
+    })
+}
+
+setEvaluacion(evaluacion, res) {
+    // Se recibe el nuevo usuario en la variable user y se crea a partir del modelo
+    Evaluacion.create(evaluacion, function(err, newEvaluacion) {
+        // sihay error se reporta
+        if (err) throw err;
+        // se retorna la informacion con el nuevo usuario creado
+        res.send({ status: 200, nEv: newEvaluacion });
+    }); 
+}
+
+//metodo para buscar todos los usuarios
+getEvaluaciones(req, res) {
+    //en el modelo User se ejecuta el find sin ninguna condicion...
+    evaluacion.find({}, (err, evaluaciones) => {
+        //en caso de haberse presentado un error se ejecuta el error
+        if (err) throw err;
+        //de lo contrario se retorna un objeto con todos los resultados
+        res.send({ allEvaluaciones: evaluaciones });
+    });
+}
+
+getEvaluacion(id, res) {
+    //en el modelo User se ejecuta el find sin ninguna condicion...
+    Evaluacion.find({ _id: id }, (err, evaluacion) => {
+        //en caso de haberse presentado un error se ejecuta el error
+        if (err) throw err;
+        //de lo contrario se retorna un objeto con todos los resultados
+        res.send({ Evaluacion: evaluacion });
+    });
+}
+
+updateEvaluacion(evaluacion, res) {
+    //optenemos los campos que queremos actualizar
+    let { id, id_actividad,} = evaluacion;
+    //actualizamos teniendo en cuenta una condicion con el operador $set
+    //https://docs.mongodb.com/manual/reference/operator/update/set/
+    evaluacion.updateOne(
+        { _id: id },
+        { $set: { id_actividad: id_actividad, } }
+    )
+        .then(rawResponse => {
+            res.send({ message: "Evaluacion  updated", raw: rawResponse });
+        })
+        .catch(err => {
+            if (err) throw err;
+        });
+}
+
+deleteEvaluacion(id, res) {
+    Evaluacion.deleteOne({ _id: id }, function(err) {
+        if (err) throw err;
+        res.send({ message: "evaluacion has been deleted" });
+    });
+}
+
+
+getEstudiante(){
+    //en el modelo User se ejecuta el find sin ninguna condicion...
+    Estudiante.find({}, (err, estudiantes)=>{
+        //en caso de haberse presentado un error se ejecuta el error
+        if(err) throw err;
+        //de lo contrario se retorna un objeto con todos los resultados
+        res.send( { allEstudiantes : estudiantes } );
+    })
+}
+
+setEstudiante(estudiante, res) {
+    // Se recibe el nuevo usuario en la variable user y se crea a partir del modelo
+    Estudiante.create(estudiante, function(err, newEstudiante) {
+        // sihay error se reporta
+        if (err) throw err;
+        // se retorna la informacion con el nuevo usuario creado
+        res.send({ status: 200, nE: newEstudiante });
+    }); 
+}
+
+//metodo para buscar todos los usuarios
+getEstudiantes(req, res) {
+    //en el modelo User se ejecuta el find sin ninguna condicion...
+    estudiante.find({}, (err, estudiantes) => {
+        //en caso de haberse presentado un error se ejecuta el error
+        if (err) throw err;
+        //de lo contrario se retorna un objeto con todos los resultados
+        res.send({ allestudiantes: estudiantes });
+    });
+}
+
+getEstudiante(id, res) {
+    //en el modelo User se ejecuta el find sin ninguna condicion...
+    Estudiante.find({ _id: id }, (err, estudiante) => {
+        //en caso de haberse presentado un error se ejecuta el error
+        if (err) throw err;
+        //de lo contrario se retorna un objeto con todos los resultados
+        res.send({ Estudiante: estudiante });
+    });
+}
+
+updateEstudiante(estudiante, res) {
+    //optenemos los campos que queremos actualizar
+    let { id, id_persona, id_grado, id_actividades } = estudiante;
+    //actualizamos teniendo en cuenta una condicion con el operador $set
+    //https://docs.mongodb.com/manual/reference/operator/update/set/
+    estudiante.updateOne(
+        { _id: id },
+        { $set: { id_persona: id_persona, id_grado: id_grado, id_actividades:id_actividades } }
+    )
+        .then(rawResponse => {
+            res.send({ message: "Estudiante updated", raw: rawResponse });
+        })
+        .catch(err => {
+            if (err) throw err;
+        });
+}
+
+deleteEstudiante(id, res) {
+    Estudiante.deleteOne({ _id: id }, function(err) {
+        if (err) throw err;
+        res.send({ message: "estudiante has been deleted" });
+    });
+}
+
+getPersona(){
+    //en el modelo User se ejecuta el find sin ninguna condicion...
+    Persona.find({}, (err, personas)=>{
+        //en caso de haberse presentado un error se ejecuta el error
+        if(err) throw err;
+        //de lo contrario se retorna un objeto con todos los resultados
+        res.send( { allPersonas : personas } );
+    })
+}
+
+setPersona(persona, res) {
+    // Se recibe el nuevo usuario en la variable user y se crea a partir del modelo
+    Persona.create(persona, function(err, newPersona) {
+        // sihay error se reporta
+        if (err) throw err;
+        // se retorna la informacion con el nuevo usuario creado
+        res.send({ status: 200, nP: newPersona });
+    }); 
+}
+
+//metodo para buscar todos los usuarios
+getPersonas(req, res) {
+    //en el modelo User se ejecuta el find sin ninguna condicion...
+    persona.find({}, (err, personas) => {
+        //en caso de haberse presentado un error se ejecuta el error
+        if (err) throw err;
+        //de lo contrario se retorna un objeto con todos los resultados
+        res.send({ allpersonas: personas });
+    });
+}
+
+getPersona(id, res) {
+    //en el modelo User se ejecuta el find sin ninguna condicion...
+    Persona.find({ _id: id }, (err, persona) => {
+        //en caso de haberse presentado un error se ejecuta el error
+        if (err) throw err;
+        //de lo contrario se retorna un objeto con todos los resultados
+        res.send({ Persona: persona });
+    });
+}
+
+updatePersona(persona, res) {
+    //optenemos los campos que queremos actualizar
+    let { id, nombre, correo, contacto } = persona;
+    //actualizamos teniendo en cuenta una condicion con el operador $set
+    //https://docs.mongodb.com/manual/reference/operator/update/set/
+    persona.updateOne(
+        { _id: id },
+        { $set: { nombre: nombre, correo: correo, contacto: contacto} }
+    )
+        .then(rawResponse => {
+            res.send({ message: "Persona updated", raw: rawResponse });
+        })
+        .catch(err => {
+            if (err) throw err;
+        });
+}
+
+deletePersona(id, res) {
+    Persona.deleteOne({ _id: id }, function(err) {
+        if (err) throw err;
+        res.send({ message: "persona has been deleted" });
+    });
+}
+
+
+
+getTemas(){
+    //en el modelo User se ejecuta el find sin ninguna condicion...
+    Tema.find({}, (err, temas)=>{
+        //en caso de haberse presentado un error se ejecuta el error
+        if(err) throw err;
+        //de lo contrario se retorna un objeto con todos los resultados
+        res.send( { allTeamas : temas } );
+    })
+}
+
+setTema(temas, res) {
+    // Se recibe el nuevo usuario en la variable user y se crea a partir del modelo
+    Tema.create(tema, function(err, newTema) {
+        // sihay error se reporta
+        if (err) throw err;
+        // se retorna la informacion con el nuevo usuario creado
+        res.send({ status: 200, nU: newTema });
+    });
+}
+
+//metodo para buscar todos los usuarios
+getTemas(req, res) {
+    //en el modelo User se ejecuta el find sin ninguna condicion...
+    tema.find({}, (err, temas) => {
+        //en caso de haberse presentado un error se ejecuta el error
+        if (err) throw err;
+        //de lo contrario se retorna un objeto con todos los resultados
+        res.send({ allTemas: temas });
+    });
+}
+
+getTema(id, res) {
+    //en el modelo User se ejecuta el find sin ninguna condicion...
+    Tema.find({ _id: id }, (err, tema) => {
+        //en caso de haberse presentado un error se ejecuta el error
+        if (err) throw err;
+        //de lo contrario se retorna un objeto con todos los resultados
+        res.send({ Tema: tema });
+    });
+}
+
+updateTema(tema, res) {
+    //optenemos los campos que queremos actualizar
+    let { id, titulo } = tema;
+    //actualizamos teniendo en cuenta una condicion con el operador $set
+    //https://docs.mongodb.com/manual/reference/operator/update/set/
+    Tema.updateOne(
+        { _id: id },
+        { $set: { titulo: titulo } }
+    )
+        .then(rawResponse => {
+            res.send({ message: "Tema updated", raw: rawResponse });
+        })
+        .catch(err => {
+            if (err) throw err;
+        });
+}
+
+deleteTema(id, res) {
+    Tema.deleteOne({ _id: id }, function(err) {
+        if (err) throw err;
+        res.send({ message: "Tema has been deleted" });
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+getSubtemas(){
+    //en el modelo User se ejecuta el find sin ninguna condicion...
+    Subtema.find({}, (err, subtemas)=>{
+        //en caso de haberse presentado un error se ejecuta el error
+        if(err) throw err;
+        //de lo contrario se retorna un objeto con todos los resultados
+        res.send( { allSubteamas : subtemas } );
+    })
+}
+
+setSubtema(subtemas, res) {
+    // Se recibe el nuevo usuario en la variable user y se crea a partir del modelo
+    Subtema.create(subtema, function(err, newSubtema) {
+        // sihay error se reporta
+        if (err) throw err;
+        // se retorna la informacion con el nuevo usuario creado
+        res.send({ status: 200, nU: newSubtema });
+    });
+}
+
+//metodo para buscar todos los usuarios
+getSubtemas(req, res) {
+    //en el modelo User se ejecuta el find sin ninguna condicion...
+    subtema.find({}, (err, subtemas) => {
+        //en caso de haberse presentado un error se ejecuta el error
+        if (err) throw err;
+        //de lo contrario se retorna un objeto con todos los resultados
+        res.send({ allSubtemas: subtemas });
+    });
+}
+
+getSubtema(id, res) {
+    //en el modelo User se ejecuta el find sin ninguna condicion...
+    Subtema.find({ _id: id }, (err, subtema) => {
+        //en caso de haberse presentado un error se ejecuta el error
+        if (err) throw err;
+        //de lo contrario se retorna un objeto con todos los resultados
+        res.send({ Subtema: subtema });
+    });
+}
+
+updateSubtema(tema, res) {
+    //optenemos los campos que queremos actualizar
+    let { id, titulo } = subtema;
+    //actualizamos teniendo en cuenta una condicion con el operador $set
+    //https://docs.mongodb.com/manual/reference/operator/update/set/
+    Subtema.updateOne(
+        { _id: id },
+        { $set: { titulo: titulo } }
+    )
+        .then(rawResponse => {
+            res.send({ message: "Subtema updated", raw: rawResponse });
+        })
+        .catch(err => {
+            if (err) throw err;
+        });
+}
+
+deleteSubtema(id, res) {
+    Subtema.deleteOne({ _id: id }, function(err) {
+        if (err) throw err;
+        res.send({ message: "Subtema has been deleted" });
+    });
+}
     /*getUsuarios(res){
       usuario.find({},(err, usuarios)=>{
         if(err) throw err;
--- bin/routes.js
+++ bin/routes.js
@@ -135,7 +135,161 @@ app.delete("/grado/:id", function(req, res) {
 
 
 
+app.post("/actividades", function(req, res) {
+  let { actividad } = req.body;
+  controller.setActividad(actividad, res);
+});
 
+//creamos la ruta users que deberá traer todos los estudiantes
+app.get("/actividades", (req, res) => {
+  //llamamos el metodo getEstudiante del objeto controller, este se encarga de buscar todos los usuarios
+  //recibe por parametros req que es igual a la consulta request(consulta) y el res que equivale al response(respuesta)
+  controller.getActividades(req, res);
+});
+
+//Traer a un Estudiante por su id
+app.get("/actividades/:id", function(req, res) {
+  let { id } = req.params;
+  controller.getActividad(id, res);
+});
+
+//Actualizar a un estudiante por su id
+app.put("/actividad:id", function(req, res) {
+  let actividad = req.body.actividad;
+  actividad.id = req.params.id;
+  controller.updateActividad(actividad, res);
+});
+
+//Eliminar a un estudiante por su id
+app.delete("/actividad/:id", function(req, res) {
+  let { id } = req.params;
+  controller.deleteActividad(id, res);
+});
+
+app.post("/evaluaciones", function(req, res) {
+  let { estudiante } = req.body;
+  controller.setEvaluacion(evaluacion, res);
+});
+
+//creamos la ruta users que deberá traer todos los estudiantes
+app.get("/evaluaciones", (req, res) => {
+  //llamamos el metodo getEstudiante del objeto controller, este se encarga de buscar todos los usuarios
+  //recibe por parametros req que es igual a la consulta request(consulta) y el res que equivale al response(respuesta)
+  controller.getEvaluaciones(req, res);
+});
+
+//Traer a un Estudiante por su id
+app.get("/evaluaciones/:id", function(req, res) {
+  let { id } = req.params;
+  controller.getevaluacion(id, res);
+});
+
+//Actualizar a un estudiante por su id
+app.put("/evaluacion/:id", function(req, res) {
+  let evaluacion = req.body.evaluacion;
+  evaluacion.id = req.params.id;
+  controller.updateEvaluacion(evaluacion, res);
+});
+
+//Eliminar a un estudiante por su id
+app.delete("/evaluacion/:id", function(req, res) {
+  let { id } = req.params;
+  controller.deleteEvaluacion(id, res);
+});
+
+app.post("/estudiantes", function(req, res) {
+  let { estudiante } = req.body;
+  controller.setEstudiante(estudiante, res);
+});
+
+//creamos la ruta users que deberá traer todos los estudiantes
+app.get("/estudiantes", (req, res) => {
+  //llamamos el metodo getEstudiante del objeto controller, este se encarga de buscar todos los usuarios
+  //recibe por parametros req que es igual a la consulta request(consulta) y el res que equivale al response(respuesta)
+  controller.getEstudiantes(req, res);
+});
+
+//Traer a un Estudiante por su id
+app.get("/estudiantes/:id", function(req, res) {
+  let { id } = req.params;
+  controller.getestudiante(id, res);
+});
+
+//Actualizar a un estudiante por su id
+app.put("/estudiantes/:id", function(req, res) {
+  let estudiante = req.body.estudiante;
+  estudiante.id = req.params.id;
+  controller.updateEstudiante(estudiante, res);
+});
+
+//Eliminar a un estudiante por su id
+app.delete("/estudiantes/:id", function(req, res) {
+  let { id } = req.params;
+  controller.deleteEstudiante(id, res);
+});
+
+
+app.post("/personas", function(req, res) {
+  let { persona } = req.body;
+  controller.setPersona(evaluacion, res);
+});
+
+//creamos la ruta users que deberá traer todos los estudiantes
+app.get("/personas", (req, res) => {
+  //llamamos el metodo getEstudiante del objeto controller, este se encarga de buscar todos los usuarios
+  //recibe por parametros req que es igual a la consulta request(consulta) y el res que equivale al response(respuesta)
+  controller.getPersonas(req, res);
+});
+
+//Traer a un Estudiante por su id
+app.get("/personas/:id", function(req, res) {
+  let { id } = req.params;
+  controller.getPersona(id, res);
+});
+
+//Actualizar a un estudiante por su id
+app.put("/persona/:id", function(req, res) {
+  let persona = req.body.persona;
+  persona.id = req.params.id;
+  controller.updatePersona(persona, res);
+});
+
+//Eliminar a un estudiante por su id
+app.delete("/persona/:id", function(req, res) {
+  let { id } = req.params;
+  controller.deletePersona(id, res);
+});
+
+app.post("/temas", function(req, res) {
+  let { tema } = req.body;
+  controller.setTemas(tema, res);
+});
+
+//creamos la ruta users que deberá traer todos los usuarios
+app.get("/temas", (req, res) => {
+  //llamamos el metodo getUser del objeto controller, este se encarga de buscar todos los usuarios
+  //recibe por parametros req que es igual a la consulta request(consulta) y el res que equivale al response(respuesta)
+  controller.getTemas(req, res);
+});
+
+//Traer a un usuario por su id
+app.get("/temas/:id", function(req, res) {
+  let { id } = req.params;
+  controller.getTema(id, res);
+});
+
+//Actualizar a un usuario por su id
+app.put("/temas/:id", function(req, res) {
+  let tema = req.body.user;
+  tema.id = req.params.id;
+  controller.updateTema(tema, res);
+});
+
+//Eliminar a un usuario por su id
+app.delete("/temas/:id", function(req, res) {
+  let { id } = req.params;
+  controller.deleteTema(id, res);
+});
 
 
 /*app.get('/usuario',function(req, res){
@@ -146,5 +300,38 @@ app.delete("/grado/:id", function(req, res) {
 res.send(usuarios)*/
 //controller.getUsuarios(res);
  // })
+
+ 
+app.post("/subtemas", function(req, res) {
+  let { subtema } = req.body;
+  controller.setSubtemas(subtema, res);
+});
+
+//creamos la ruta users que deberá traer todos los usuarios
+app.get("/subtemas", (req, res) => {
+  //llamamos el metodo getUser del objeto controller, este se encarga de buscar todos los usuarios
+  //recibe por parametros req que es igual a la consulta request(consulta) y el res que equivale al response(respuesta)
+  controller.getSubtemas(req, res);
+});
+
+//Traer a un usuario por su id
+app.get("/subtemas/:id", function(req, res) {
+  let { id } = req.params;
+  controller.getSubtema(id, res);
+});
+
+//Actualizar a un usuario por su id
+app.put("/subtemas/:id", function(req, res) {
+  let subtema = req.body.user;
+  subtema.id = req.params.id;
+  controller.updateSubtema(subtema, res);
+});
+
+//Eliminar a un usuario por su id
+app.delete("/subtemas/:id", function(req, res) {
+  let { id } = req.params;
+  controller.deleteSubtema(id, res);
+});
+
 
   exports.app=app;
